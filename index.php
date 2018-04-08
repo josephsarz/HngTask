96f7c356afdf80ef2da74bfc4479b286dcc35895
femicodes <aghedojoe007@gmail.com>
2018-04-08 21:25:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Aghedo Joseph Femi</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+
+		<meta name="author" content="Aghedo Joseph Femi">
+		<meta name="description" content="Android Developer">
+
+
+
+		<script>
+			
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		  ga('create', 'UA-77783023-1', 'auto');
+		  ga('send', 'pageview');
+		  
+		  function _gaLt(t){if(ga.hasOwnProperty("loaded")&&1==ga.loaded){for(var e=t.srcElement||t.target;e&&("undefined"==typeof e.tagName||"a"!=e.tagName.toLowerCase()||!e.href);)e=e.parentNode;if(e&&e.href){var a=e.href;if(-1==a.indexOf(location.host)&&!a.match(/^javascript\:/i)){var n=(e.target&&!e.target.match(/^_(self|parent|top)$/i)?e.target:!1,!1),o=function(){n||(n=!0,window.location.href=a)};e.target&&!e.target.match(/^_(self|parent|top)$/i)?ga("send","event","Outgoing Links",a,document.location.pathname+document.location.search):(ga("send","event","Outgoing Links",a,document.location.pathname+document.location.search,{hitCallback:o}),setTimeout(o,1e3),t.preventDefault?t.preventDefault():t.returnValue=!1)}}}}var w=window;w.addEventListener?w.addEventListener("load",function(){document.body.addEventListener("click",_gaLt,!1)},!1):w.attachEvent&&w.attachEvent("onload",function(){document.body.attachEvent("onclick",_gaLt)});
+		  
+		</script>
+	</head>
+	<body class="loading">
+		<div id="wrapper">
+			<div id="bg"></div>
+			<div id="overlay"></div>
+			<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+					
+					<?php
+						echo "Time " . date("h:i:sa");
+					?>
+
+							<!-- <p>Time: <span id="datetime"></span></p> -->
+						<h1>Aghedo Joseph Femi</h1>
+						<p>Android Developer &nbsp;&bull;&nbsp; Technical Writer &nbsp;&bull;&nbsp; Part Time UI/UX Designer</p>
+					
+						<nav>
+							<ul>
+								<li><a href="https://twitter.com/femicodes" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://linkedin.com/in/aghedo-joseph" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+								<li><a href="https://stackoverflow.com/users/7046279/joseph-sarz" class="icon fa-stack-overflow"><span class="label">StackOverflow</span></a></li>
+								<li><a href="https://github.com/josephsarz" class="icon fa-github"><span class="label">Github</span></a></li>
+								<li><a href="mailto:aghedojoe007@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+							</ul>
+						</nav>
+					</header>
+
+			</div>
+		</div>
+		<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+		<script>
+			
+	// var dt = new Date();
+	// document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+			window.onload = function() { document.body.className = ''; }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+		</script>
+	</body>
+</html>
+Hi, great job so far.
+You were meant to use php to get the time, not JS
+Also, the webpage is meant to run on your localhost via wamp or xampp
+
+okay thanks, noted.
+So do the right thing then let me know
